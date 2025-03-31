812ee7c3fb275a18277a767a42ee181e5a09a11c
jehieli19 <jehielixool@gmail.com>
2025-03-31 03:18:40 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
-﻿using System; 
-// Importa el espacio de nombres System para usar Console y Math
+﻿
+using System; 
 
-public class AreaTriangulo 
-// Define una clase llamada AreaTriangulo
-{
+public class MultiplicarPorDos 
+ {
     public static void Main(string[] args) 
-    // Método principal, punto de entrada del programa
     {
-        Console.WriteLine("Ingrese los lados del triángulo (a, b, c):"); // Pide al usuario los lados del triángulo
-        double a = Convert.ToDouble(Console.ReadLine()); // Lee el lado a y lo convierte a double
-        double b = Convert.ToDouble(Console.ReadLine()); // Lee el lado b y lo convierte a double
-        double c = Convert.ToDouble(Console.ReadLine()); // Lee el lado c y lo convierte a double
+        Console.WriteLine("Ingrese un número :"); 
+        int numero = Convert.ToInt32(Console.ReadLine()); 
 
-        double p = (a + b + c) / 2; // Calcula el semiperímetro
-        double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-         // Calcula el área usando la fórmula de Herón
-
-        Console.WriteLine("El área del triángulo es: " + area); 
-        // Muestra el área calculada
+        int resultado = numero * 2; 
+        Console.WriteLine("El resultado de multiplicar por 2 es: " + resultado); 
     }
 }
